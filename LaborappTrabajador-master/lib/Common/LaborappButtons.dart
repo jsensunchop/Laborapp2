01fa0ce8c23eb51f3deb32111fc7acb24e4e627c
David <dsfajardob@unal.edu.co>
2020-01-08 12:39:56 -0500

    All buttons added

---
+++ LaborappTrabajador-master/lib/Common/LaborappButtons.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:laborapp_trabajador/Common/ColorPalette.dart';
+import 'package:laborapp_trabajador/Util/UtilMethods.dart';
+
+class LaboraapButtons {
+  int getButtonTextColor(int colorCode) {
+    return colorCode == ColorPalette.yellowApp
+        ? ColorPalette.strongGeryApp
+        : ColorPalette.softGrayApp;
+  }
+
+  Widget NormalButton(
+      {String inText,
+      int colorCode,
+      Function buttonFunction,
+      BuildContext context}) {
+
+    double buttonHeight = getHeightWithoutSafeArea(context) * 0.06;
+    double buttonWidth = getFullScreenWidth(context) * 0.5;
+    if(inText.split(" ").length > 1)
+    {
+      buttonWidth = getFullScreenWidth(context) * 0.8;
+    }
+
+    var buttonContainer = Container(
+      height: buttonHeight,
+      width: buttonWidth,
+      child: FlatButton(
+        color: Color(colorCode),
+        textColor: Color(getButtonTextColor(colorCode)),
+        onPressed: () => buttonFunction(),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        child: Text(inText.toUpperCase(),
+            style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.w800)),
+      ),
+    );
+    return buttonContainer;
+  }
+
+  Widget PopUpButton(
+      {String inText,
+        int colorCode,
+        Function buttonFunction,
+        BuildContext context}) {
+
+    double buttonHeight = getHeightWithoutSafeArea(context) * 0.06;
+    double buttonWidth = getFullScreenWidth(context) * 0.6;
+    var button;
+    if(colorCode != ColorPalette.strongGeryApp){
+      button = OutlineButton(
+        color: Color(colorCode),
+        textColor: Colors.white,
+        borderSide: BorderSide(color: Colors.white,width: 2.0),
+        onPressed: () => buttonFunction(),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        child: Text(inText.toUpperCase(),
+            style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.w800)),
+      );
+    }else{
+      button = FlatButton(
+        color: Color(colorCode),
+        textColor: Color(getButtonTextColor(colorCode)),
+        onPressed: () => buttonFunction(),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        child: Text(inText.toUpperCase(),
+            style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.w800)),
+      );
+    }
+
+    var buttonContainer = Container(
+      height: buttonHeight,
+      width: buttonWidth,
+      child: button
+    );
+    return buttonContainer;
+  }
+}
--- LaborappTrabajador-master/lib/Tests/testScreenOne.dart
+++ LaborappTrabajador-master/lib/Tests/testScreenOne.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:laborapp_trabajador/Common/ColorPalette.dart';
 import 'package:laborapp_trabajador/Common/LaboraAppBar.dart';
+import 'package:laborapp_trabajador/Common/LaborappButtons.dart';
 
 class testScreenOne extends StatefulWidget {
   @override
@@ -7,11 +9,88 @@ class testScreenOne extends StatefulWidget {
 }
 
 class _testScreenOneState extends State<testScreenOne> {
+  sayHi() {
+    print("Hi!");
+    print("REGISTRESE".length);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: LaborAppBar().build(context),
-
+      body: Container(
+        width: 500,
+        height: 800,
+        color: Colors.blue,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            LaboraapButtons().NormalButton(
+                inText: "REGISTRESE",
+                colorCode: ColorPalette.yellowApp,
+                buttonFunction: () {
+                  sayHi();
+                },
+                context: context),
+            LaboraapButtons().NormalButton(
+                inText: "ENTRAR",
+                colorCode: ColorPalette.strongGeryApp,
+                buttonFunction: () {
+                  sayHi();
+                },
+                context: context),
+            LaboraapButtons().NormalButton(
+                inText: "INGRESAR",
+                colorCode: ColorPalette.yellowApp,
+                buttonFunction: () {
+                  sayHi();
+                },
+                context: context),
+            LaboraapButtons().NormalButton(
+                inText: "CONFIRMAR",
+                colorCode: ColorPalette.strongGeryApp,
+                buttonFunction: () {
+                  sayHi();
+                },
+                context: context),
+            LaboraapButtons().NormalButton(
+                inText: "SIGUIENTE",
+                colorCode: ColorPalette.yellowApp,
+                buttonFunction: () {
+                  sayHi();
+                },
+                context: context),
+            LaboraapButtons().NormalButton(
+                inText: "SUBIR",
+                colorCode: ColorPalette.strongGeryApp,
+                buttonFunction: () {
+                  sayHi();
+                },
+                context: context),
+            LaboraapButtons().NormalButton(
+                inText: "APLICAR A OFERTAS",
+                colorCode: ColorPalette.yellowApp,
+                buttonFunction: () {
+                  sayHi();
+                },
+                context: context),
+            LaboraapButtons().PopUpButton(
+                inText: "TOMO ESTA OFERTA",
+                colorCode: ColorPalette.strongGeryApp,
+                buttonFunction: () {
+                  sayHi();
+                },
+                context: context),
+            LaboraapButtons().PopUpButton(
+                inText: "ENTENDIDO",
+                colorCode: ColorPalette.yellowApp,
+                buttonFunction: () {
+                  sayHi();
+                },
+                context: context),
+          ],
+        ),
+      ),
     );
   }
 }
